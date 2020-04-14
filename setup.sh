9197866751d7a47cd8504e5295ba5d6de9a7feba
Eva van Rooijen <evav.rooijen@live.nl>
2020-04-14 20:34:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"evav.rooijen@live.nl\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
